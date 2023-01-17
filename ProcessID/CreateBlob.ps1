b604d8b281f3a8391521809446fcf4fc71ca1bbb
Jon Shaulis <jon@mscreations.net>
2023-01-16 22:49:51 -0500

    Add ProcessID solution

---
+++ ProcessID/CreateBlob.ps1
@@ -0,0 +1,6 @@
+﻿$FilePath = 'D:\Users\jon\Nextcloud\!System Folders\Documents\Source\ProcessID\bin\Release\ProcessID.dll'
+$File = [System.IO.File]::ReadAllBytes($FilePath)
+$blob = [System.Convert]::ToBase64String($File)
+echo @"
+`$blob = '$blob'
+"@ | Out-File 'D:\Users\jon\Nextcloud\!System Folders\Documents\Source\ProcessID\bin\Release\ProcessIDBlob.ps1'
---
+++ ProcessID/SerialHelp.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Management;
+
+namespace ProcessID
+{
+    public class SerialHelp
+    {
+        public static string GetMacropadDataSerialPort()
+        {
+            foreach (string Port in System.IO.Ports.SerialPort.GetPortNames())
+            {
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE Name LIKE '%" + Port + "%'");
+                ManagementObject mo = searcher.Get().OfType<ManagementObject>().FirstOrDefault();
+                object[] args = new object[] { new string[] { "DEVPKEY_Device_BusReportedDeviceDesc" }, null };
+                mo.InvokeMethod("GetDeviceProperties", args);
+                ManagementBaseObject[] mbos = (ManagementBaseObject[])args[1];
+                if (mbos.Length > 0)
+                {
+                    PropertyData data = mbos[0].Properties.OfType<PropertyData>().FirstOrDefault(p => p.Name == "Data");
+                    if (data != null && data.Value.ToString().Contains("CDC2"))
+                    {
+                        return Port;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
